163969e39372971a6d141e1665f41999147c9260
harsh-u-simform <harsh.ughreja@simformsolutions.com>
2024-07-21 01:35:22 +0530

    bug fix

--- fusionworks/settings.py
+++ fusionworks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i7xqtn_j$s0o5ps$numaoehv1d26kzytr)gz-9+64j$0t#$is&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://fusionworks-dev-llm.cloudjet.site']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
